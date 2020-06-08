02c211b609db43828a67b7f05cb12af2fc26b83f
Hndsm1917 <45174726+Hndsm1917@users.noreply.github.com>
2020-06-08 13:35:19 +0300

    Add files via upload

---
+++ Function2.js
@@ -0,0 +1,10 @@
+let array = ['shoes','shirts', 'socks', 'sweaters']
+
+function arrayImprover() {
+    let elem1 = array.pop()
+    console.log(elem1)
+
+    let elem2 = array.pop()
+    console.log(elem2)
+}
+arrayImprover()
